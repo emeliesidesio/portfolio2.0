6916cb59fb54eb20ec23050916f9c8ecc1545cbe
Emelie Sidesiö <esidesio@gmail.com>
2019-10-18 21:07:55 +0200

    create home page component

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LottieControl from "./animation/lottieControl.js";
+import "./index.css";
+
+export default class Home extends React.Component {
+    render() {
+      return (
+        <div className="home">
+          <div className="header">Hej</div>
+          <div className="content"></div>
+          <div className="footer"></div>
+        </div>
+      )
+    }
+}
