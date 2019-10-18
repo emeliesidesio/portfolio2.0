b12b528e662e280c418a4b80242017ba8c5e34cc
Emelie Sidesiö <esidesio@gmail.com>
2019-10-18 21:08:04 +0200

    install router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={Home} />
+          </div>
+        </BrowserRouter>
+      </div>
   );
 }
 
