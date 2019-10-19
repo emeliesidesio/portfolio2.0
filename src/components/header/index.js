d1a1287f26b8ae00d32d3f46489bc1e7235ce9e2
Emelie Sidesiö <esidesio@gmail.com>
2019-10-19 20:42:27 +0200

    header component

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="heading1">Hi, I'm Emelie</div>
+      <div className="subHeading">Front end developer
+      & graphic designer</div>
+    </div>
+  )
+}
+
+export default Header
