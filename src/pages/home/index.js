492665ca2602c855308179e0f3a60d5ae96650c8
Emelie Sidesiö <esidesio@gmail.com>
2019-10-19 20:44:25 +0200

    update home component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import LottieControl from "components/lottieControl";
-import "./index.css";
+import Header from "components/header";
+import Content from "components/content";
+import Footer from "components/footer";
 
 export default class Home extends React.Component {
     render() {
       return (
         <div className="home">
-          <div className="header">
-            <LottieControl />
-          </div>
-          <div className="content"></div>
-          <div className="footer"></div>
+          <Header />
+          <Content />
+          <Footer />
         </div>
       )
     }
